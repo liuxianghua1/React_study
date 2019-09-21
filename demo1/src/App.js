532dbdd7c5b94bea466d9c89adba695c6f7fccdf
liuxianghua1 <3374312139@qq.com>
2019-09-21 14:09:11 +0800

    first commit

---
+++ demo1/src/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Hello React!!!!语法很优美!!!
+            </div>
+        );
+    }
+}
+
+export default App;
